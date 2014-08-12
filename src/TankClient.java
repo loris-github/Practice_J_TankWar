5cb4cb97471a2f1113409db1ecc89a436f9bfcc3
loris-github <laotao200437@gmail.com>
2014-08-12 22:10:03 +0800

    make the frame better
    the frame is not resizeable now

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,11 +1,34 @@
 import java.awt.*;
+import java.awt.event.*;
 
 public class TankClient extends Frame {
 	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(50, 50, 30, 30);
+		g.setColor(c);
+		
+	}
+
 	public void lauchFrame(){
 		this.setLocation(400,300);
 		this.setSize(800,600);
-		setVisible(true);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter(){
+
+			@Override
+			public void windowClosing(WindowEvent e) {				
+				System.exit(0);
+			}
+			
+		});
+		this.setBackground(Color.DARK_GRAY);
+		this.setVisible(true);
+		this.setResizable(false);
+		
 	}
 
 	public static void main(String[] args) {

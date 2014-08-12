14260839000e1d98e698619316a99007080efd01
loris-github <laotao200437@gmail.com>
2014-08-12 22:16:07 +0800

    tank can move
    tank can move

--- src/Tank.java
+++ src/Tank.java
@@ -2,8 +2,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Tank {
+	public static final int XSPEED = 5;
+	public static final int YSPEED = 5;
 	
-	int x , y;
+	private int x , y;
+	
+	private boolean bL = false,bU = false,bR = false,bD = false;
+	private enum Direction{L,LU,U,RU,R,RD,D,LD,STOP};
+	
+	private Direction dir = Direction.STOP;
 	
 	public Tank(int x, int y) {
 		super();
@@ -16,6 +23,42 @@ public class Tank {
 		g.setColor(Color.RED);
 		g.fillOval(x, y, 30, 30);
 		g.setColor(c);
+		move();
+	}
+	
+	void move(){
+		switch(dir){
+			case L:
+				x-= XSPEED;
+				break;
+			case LU:
+				x -= XSPEED;
+				y -= YSPEED;
+				break;
+			case U:
+				y -= YSPEED;
+				break;
+			case RU:
+				x += XSPEED;
+				y -= YSPEED;
+				break;
+			case R:
+				x += XSPEED;
+				break;
+			case RD:
+				x += XSPEED;
+				y += YSPEED;
+				break;
+			case D:
+				y += YSPEED;
+				break;
+			case LD:
+				x -= XSPEED;
+				y += YSPEED;
+				break;
+			case STOP:
+				break;
+		}
 	}
 	
 	public void keyPressed(KeyEvent e){
@@ -23,19 +66,32 @@ public class Tank {
 		
 		switch(key){
 		case KeyEvent.VK_LEFT :
-			x -= 5;
+			bL = true;
 			break;
 		case KeyEvent.VK_UP:
-			y -= 5;
+			bU = true;
 			break;
 		case KeyEvent.VK_RIGHT:
-			x += 5;
+			bR = true;
 			break;					
 		case KeyEvent.VK_DOWN:
-			y += 5;
+			bD = true;
 			break;		
 		}
+		
+		locateDirection();
 
 	}
 
+	void locateDirection(){
+		if(bL && !bU && !bR && !bD) dir = Direction.L;
+		else if(bL && bU && !bR && !bD) dir = Direction.LU;
+		else if(!bL && bU && !bR && !bD) dir = Direction.U;
+		else if(!bL && bU && bR && !bD) dir = Direction.RU;
+		else if(!bL && !bU && bR && !bD) dir = Direction.R;
+		else if(!bL && !bU && bR && bD) dir = Direction.RD;
+		else if(!bL && !bU && !bR && bD) dir = Direction.D;
+		else if(bL && bU && bR && !bD) dir = Direction.LD;
+		else if(!bL && !bU && !bR && !bD) dir = Direction.STOP;
+	}
 }
